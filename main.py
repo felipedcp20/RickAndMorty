9674f13d0372ec31d1d16852cbe3ea2bc4aef6cf
Felipe <felipedcp@gmail.com>
2022-02-13 19:05:47 -0500

    endpoint location and episode

--- main.py
+++ main.py
@@ -5,13 +5,13 @@ from os import remove
 import time
 
 from fastapi import FastAPI
-
+from typing import Optional
 
 app = FastAPI()
 
 
-@app.get('/')
-def character(name: str ):
+@app.get('/name')
+def character(name: Optional[str] = None ):
     
     url = 'https://rickandmortyapi.com/api/character/'
     arg = {'name': name}
@@ -26,10 +26,53 @@ def character(name: str ):
         namefile = time.ctime()  
             
         with zipfile.ZipFile(f' character {namefile}.zip','w') as fzip:
-            fzip.write('data.json')   
+            fzip.write('data.json')
+            remove('data.json')   
             
         
         return response_json
     
+@app.get('/location')   
+def location(location: Optional[str] = None ):
     
+    url = 'https://rickandmortyapi.com/api/location/'
+    arg = {'name': location}
+    response = requests.get(url, params=arg)
     
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        
+        with open('data.json', 'w') as file:
+            json.dump(response_json,file,indent=4)
+        
+        namefile = time.ctime()  
+            
+        with zipfile.ZipFile(f' location {namefile}.zip','w') as fzip:
+            fzip.write('data.json') 
+            remove('data.json')   
+            
+        
+        return response_json
+    
+@app.get('/episode') 
+def episode(episode: Optional[str] = None ):
+    
+    url = 'https://rickandmortyapi.com/api/episode/'
+    arg = {'name': episode}
+    response = requests.get(url, params=arg)
+    
+    if response.status_code == 200:
+        response_json = json.loads(response.text)
+        
+        with open('data.json', 'w') as file:
+            json.dump(response_json,file,indent=4)
+        
+        namefile = time.ctime()  
+            
+        with zipfile.ZipFile(f' episode {namefile}.zip','w') as fzip:
+            fzip.write('data.json') 
+            remove('data.json')   
+            
+        
+        return response_json
+                
